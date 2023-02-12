d959ddb1d433d4308499f3dcaaa62bb1e42ad840
codr7 <codr7@protonmail.com>
2023-02-12 07:23:46 +0100

    Only log on debug.

--- fuse.go
+++ fuse.go
@@ -34,7 +34,11 @@ func (self *Vm) FuseAddInt(startPc Pc, prevOp *Op) int {
 			prevOp.Id() == PUSH_INT_OP &&
 			op.Id() == CALL_PRIM_OP &&
 			op.CallPrim() == self.AbcLib.AddPrim.tag {
-			fmt.Fprintf(self.Stdout, "Fusing %v ADD_INT\n", pc);
+
+			if self.Debug {
+				fmt.Fprintf(self.Stdout, "Fusing %v ADD_INT\n", pc);
+			}
+			
 			*op = AddIntOp(prevOp.PushInt())
 			*prevOp = NOp()
 			count++
@@ -67,7 +71,11 @@ func (self *Vm) FuseEqInt(startPc Pc, prevOp *Op) int {
 			prevOp.Id() == PUSH_INT_OP &&
 			op.Id() == CALL_PRIM_OP &&
 			op.CallPrim() == self.AbcLib.EqPrim.tag {
-			fmt.Fprintf(self.Stdout, "Fusing %v EQ_INT\n", pc);
+
+			if self.Debug {
+				fmt.Fprintf(self.Stdout, "Fusing %v EQ_INT\n", pc);
+			}
+			
 			*op = EqIntOp(prevOp.PushInt())
 			*prevOp = NOp()
 			count++
@@ -95,7 +103,11 @@ func (self *Vm) FuseGoto(startPc Pc, prevOp *Op) int {
 		if prevOp != nil &&
 			prevOp.Id() == GOTO_OP &&
 			(op.Id() == GOTO_OP || op.Id() == RET_OP || op.Id() == STOP_OP) {
-			fmt.Fprintf(self.Stdout, "Fusing %v GOTO\n", pc);
+
+			if self.Debug {
+				fmt.Fprintf(self.Stdout, "Fusing %v GOTO\n", pc);
+			}
+
 			*prevOp = *op
 			count++
 		}
@@ -133,7 +145,11 @@ func (self *Vm) FuseGtInt(startPc Pc, prevOp *Op) int {
 			prevOp.Id() == PUSH_INT_OP &&
 			op.Id() == CALL_PRIM_OP &&
 			op.CallPrim() == self.AbcLib.GtPrim.tag {
-			fmt.Fprintf(self.Stdout, "Fusing %v GT_INT\n", pc);
+
+			if self.Debug {
+				fmt.Fprintf(self.Stdout, "Fusing %v GT_INT\n", pc);
+			}
+			
 			*op = GtIntOp(prevOp.PushInt())
 			*prevOp = NOp()
 			count++
@@ -161,7 +177,11 @@ func (self *Vm) FuseNop(startPc Pc, prevOp *Op) int {
 		if prevOp != nil &&
 			((prevOp.Id() == GOTO_OP && prevOp.GotoPc() < pc+1) || prevOp.Id() == NOP) &&
 			op.Id() == NOP {
-			fmt.Fprintf(self.Stdout, "Fusing %v NOP\n", pc);
+
+			if self.Debug {
+				fmt.Fprintf(self.Stdout, "Fusing %v NOP\n", pc);
+			}
+			
 			*prevOp = GotoOp(pc+1)
 			count++
 		}
@@ -189,7 +209,10 @@ func (self *Vm) FuseRec(startPc Pc, prevOp *Op) int {
 		}
 
 		if prevOp != nil && prevOp.Id() == CALL_FUN_OP && op.Id() == RET_OP {
-			fmt.Fprintf(self.Stdout, "Fusing %v REC\n", pc);
+			if self.Debug {
+				fmt.Fprintf(self.Stdout, "Fusing %v REC\n", pc);
+			}
+			
 			*prevOp = RecOp(self.Tags[prevOp.CallFun()].d.(*Fun))
 			count++
 		}
@@ -221,7 +244,11 @@ func (self *Vm) FuseSubInt(startPc Pc, prevOp *Op) int {
 			prevOp.Id() == PUSH_INT_OP &&
 			op.Id() == CALL_PRIM_OP &&
 			op.CallPrim() == self.AbcLib.SubPrim.tag {
-			fmt.Fprintf(self.Stdout, "Fusing %v SUB_INT\n", pc);
+
+			if self.Debug {
+				fmt.Fprintf(self.Stdout, "Fusing %v SUB_INT\n", pc);
+			}
+			
 			*op = SubIntOp(prevOp.PushInt())
 			*prevOp = NOp()
 			count++
